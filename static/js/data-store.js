9bf9e3ea6fd5b634532aae8f48a3d736b89272d8
SullyJR <sulca807@student.otago.ac.nz>
2023-09-20 12:50:21 +1200

    added session storage closes #4

---
+++ static/js/data-store.js
@@ -0,0 +1,29 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
+ */
+export const dataStore = Vuex.createStore({
+
+	state () {
+		// signed in customer
+		customer: null;
+		
+		// the shopping cart items
+		items: null;
+	},
+
+	mutations: {
+
+		// user signs in
+		signIn(state, customer) {
+			state.customer = customer;
+			state.items = new Array();
+		}
+
+	},
+
+	// add session storage persistence
+	plugins: [window.createPersistedState({storage: window.sessionStorage})]
+
+});
+
